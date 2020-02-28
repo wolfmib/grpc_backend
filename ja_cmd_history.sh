3f63982adb687bb3b6989f88342db38750835cbf
Johnny_Wick <wolfmib@gmail.com>
2020-02-28 18:54:03 +0800

    add

--- ja_cmd_history.sh
+++ ja_cmd_history.sh
@@ -35,3 +35,5 @@ echo "handle the display find.() display uuid format "
 curl https://github.com/mongodb/mongo-csharp-driver/blob/master/uuidhelpers.js --out uuidhelpers.js
 
 git submodule add git@gitlab.com:Johnny_Wick/ja_gitlab_grpc_proto.git proto
+rm -rf .git/modules/proto/
+rm .gitmodules
