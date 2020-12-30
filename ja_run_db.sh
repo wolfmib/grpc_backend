d391d5ed3b049a114b11edcc5ce591056db9daad
johnny <johnny@johnnys-MacBook-Pro.local>
2020-12-30 18:38:04 +0800

    updated_ja_run_db_sh

--- ja_run_db.sh
+++ ja_run_db.sh
@@ -13,11 +13,30 @@ echo [Jason]: Running ja_mongodb by type mongod!!!!!!!!!!!!!!
 echo "[Jason]: Type now!"
 echo "Example"
 current_path=$(pwd)
-echo "mongod --dbpath $current_path/ja_mongodb"
+echo "mongod --dbpath $current_path/ja_mongodb , Press Enter to key one by one.."
+
 read ARGUMENTS
 set -- $ARGUMENTS
-# echo $1 mongod
-# echo $2 --dbpath
-# echo $3 /Users/johnny_hung/Desktop/grpc_backend/ja_mongodb
 
-run_cmd $1 $2 $3
+echo "Type: mongod"
+read ja_cmd_var
+echo "You key:  $ja_cmd_var"
+echo 
+
+echo "Type: --dbpath"
+read ja_option_var
+echo "You key:  $ja_option_var"
+echo 
+
+
+echo "Type: $current_path/ja_mongodb"
+read ja_path_var
+echo "You key:  $ja_path_var"
+echo 
+
+
+#echo $1 mongod
+#echo $2 --dbpath
+#echo $3 /Users/johnny_hung/Desktop/grpc_backend/ja_mongodb
+
+run_cmd $ja_cmd_var $ja_option_var $ja_path_var
